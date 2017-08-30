5018ab368e9f8fc27bf5ff41e6ec4dbe94a398d6
Zachary Voase <zack@meat.io>
2017-08-29 21:40:05 -0700

    Allow getting the autoupdate status of a Solver in C.

--- lib/amoebalib.cpp
+++ lib/amoebalib.cpp
@@ -2,6 +2,11 @@
 
 #include "amoeba.h"
 
-extern "C" __attribute__((used)) double am_getstrength(am_Constraint *constraint) {
-	return constraint->strength;
+extern "C" {
+	__attribute__((used)) int am_getautoupdate(am_Solver *solver) {
+		return solver->auto_update;
+	}
+	__attribute__((used)) double am_getstrength(am_Constraint *constraint) {
+		return constraint->strength;
+	}
 }
